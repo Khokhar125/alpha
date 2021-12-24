26e4d31e3d223ad2c011453049df3ec823b04118
Abdul Hannan Khokhar <abdulhannankhokhar125@gmail.com>
2021-12-24 18:52:35 +0500

    Setup Rails Application And Setup home And about page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+
+
+class PagesController < ApplicationController
+
+def homepage
+
+end
+
+def about
+
+end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+   get 'pages/homepage' ,  to: 'pages#homepage'
+
+   get 'pages/about' ,  to: 'pages#about'
+
+
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
