cb99a0afb824a7e7395e4b819fa9101bd45f981b
Abdul Hannan Khokhar <abdulhannankhokhar125@gmail.com>
2021-12-29 18:40:22 +0500

    In This, We Save User Data In Database But Not Show On Broswer.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
-class Articles < ActiveRecord::Base
+class Article < ActiveRecord::Base
 validates :title,  presence: true, length: {minimum:10, maximum:50 }
 validates :description, presence: true, length: {minimum:10, maximum:200}
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-   get 'pages/homepage' ,  to: 'pages#homepage'
+   root 'pages#homepage'
 
-   get 'pages/about' ,  to: 'pages#about'
+   get 'about' ,  to: 'pages#about'
+
+   resources :articles
 
 
   # Defines the root path route ("/")
