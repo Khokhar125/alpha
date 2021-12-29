fbaec7db973c0c958cc9e0f6f8c50106de29702f
Abdul Hannan Khokhar <abdulhannankhokhar125@gmail.com>
2021-12-29 15:17:53 +0500

    In This, We Add Data, Edit Data And Delete Data in Database. And Also Have The Validations Checks.

--- app/models/articles.rb
+++ app/models/articles.rb
@@ -1,6 +1,7 @@
 class Articles < ActiveRecord::Base
 validates :title,  presence: true, length: {minimum:10, maximum:50 }
 validates :description, presence: true, length: {minimum:10, maximum:200}
+
 #But it also add data in database of just one words.
 #For This We Also add length check on both attributes.
 
